e6ff4446437ba62614cf90dc57b35eea0004705b
George <71434803+george-swift@users.noreply.github.com>
2022-01-29 00:26:19 +0100

    Export shared styles

---
+++ styles/index.ts
@@ -0,0 +1,7 @@
+import theme from './theme'
+import mixins from './mixins'
+import media from './media'
+import Container from './Container'
+import GlobalStyle from './GlobalStyle'
+
+export { theme, mixins, media, GlobalStyle, Container }

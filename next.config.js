7e867b3a97a60ca7048d51fd12f267e146965a6a
George <71434803+george-swift@users.noreply.github.com>
2022-01-31 23:13:50 +0100

    Declare module and support image domains

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  images: {
+    domains: [
+      'avatars.githubusercontent.com',
+      'avatars2.githubusercontent.com',
+    ],
+  },
 }

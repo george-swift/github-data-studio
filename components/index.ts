c52280c7c81e776d794e1742781cca8ec9153097
George <71434803+george-swift@users.noreply.github.com>
2022-01-31 23:26:16 +0100

    Add head to exported components

---
+++ components/index.ts
@@ -0,0 +1,19 @@
+import Head from './Head'
+import RateLimit from './RateLimit'
+import UserInfo from './UserInfo'
+import Charts from './Charts'
+import Repositories from './Repositories'
+import Footer from './Footer'
+import Alert from './Alert'
+import Corner from './Corner'
+
+export {
+  Head,
+  RateLimit,
+  UserInfo,
+  Charts,
+  Repositories,
+  Footer,
+  Alert,
+  Corner,
+}

53d167d19fd0bc06bfd680c3866827c5095af046
George <71434803+george-swift@users.noreply.github.com>
2022-01-31 23:09:44 +0100

    Add alert component

---
+++ components/Alert.tsx
@@ -0,0 +1,40 @@
+import { MarkGithubIcon } from '@primer/octicons-react'
+import { Head } from '../components'
+import AlertStyles from './styles/AlertStyles'
+
+interface AlertProps {
+  error: {
+    type: number
+  }
+}
+
+const Alert = ({ error }: AlertProps) => (
+  <AlertStyles>
+    <Head title="Github Portfolio" />
+    <MarkGithubIcon size="large" />
+    <h1>Github Porfolio</h1>
+    {error && (
+      <div>
+        {error.type === 403 ? (
+          <p>
+            Oops! You have exceeded the{' '}
+            <a
+              href="https://developer.github.com/v3/rate_limit/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              rate limit
+            </a>
+            ! Try again later
+          </p>
+        ) : error.type === 404 ? (
+          <p>User not found!</p>
+        ) : (
+          <p>Something went wrong. Try again later!</p>
+        )}
+      </div>
+    )}
+  </AlertStyles>
+)
+
+export default Alert
